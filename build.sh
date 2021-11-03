8d50bdacea4cc0d58690ac6dd2f5f6518aa74834
Philip Washington Sorst <philip@sorst.net>
2021-11-03 11:31:47 +0100

    Changing tags to latest

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-docker build -t dontdrinkandroot/php:$BRANCH-common -f common.Dockerfile . \
-&& docker build -t dontdrinkandroot/php:$BRANCH-test -f test.Dockerfile --build-arg FROM=dontdrinkandroot/php:$BRANCH-common . \
-&& docker build -t dontdrinkandroot/php:$BRANCH-dev -f dev.Dockerfile --build-arg FROM=dontdrinkandroot/php:$BRANCH-test .
+docker build -t dontdrinkandroot/php/$BRANCH/common:latest -f common.Dockerfile . \
+&& docker build -t dontdrinkandroot/php/$BRANCH/test:latest -f test.Dockerfile --build-arg FROM=dontdrinkandroot/php/$BRANCH/common:latest . \
+&& docker build -t dontdrinkandroot/php/$BRANCH/dev:latest -f dev.Dockerfile --build-arg FROM=dontdrinkandroot/php/$BRANCH/test:latest .
