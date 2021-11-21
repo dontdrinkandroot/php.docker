df299e82770038ab9bb91a4e57e44dcac377b39e
Philip Washington Sorst <philip@sorst.net>
2021-11-22 00:03:29 +0100

    Improving build and deploy commands

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-# docker build -t dontdrinkandroot/php:$BRANCH.common -f common.Dockerfile .
-docker push dontdrinkandroot/php:$BRANCH.common
+docker push dontdrinkandroot/php:$BRANCH-common \
+&& docker push dontdrinkandroot/php:$BRANCH-test \
+&& docker push dontdrinkandroot/php:$BRANCH-dev
