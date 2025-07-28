6c27625319b0aa46bceb3309cb0bed31572d8cfe
Philip Washington Sorst <philip@sorst.net>
2025-07-28 11:01:12 +0200

    refactor: Adding debug output

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
 
-if [ -n "$MEMORY_LIMIT" ]; then
-    echo "memory_limit = $MEMORY_LIMIT" > /etc/php84/conf.d/05_memory_limit.ini
-else
-    echo "memory_limit = 512M" > /etc/php84/conf.d/05_memory_limit.ini
-fi
+MEMORY_LIMIT=${MEMORY_LIMIT:-512M}
+UPLOAD_MAX_FILESIZE=${UPLOAD_MAX_FILESIZE:-128M}
+POST_MAX_SIZE=${POST_MAX_SIZE:-128M}
 
-if [ -n "$UPLOAD_MAX_FILESIZE" ]; then
-    echo "upload_max_filesize = $UPLOAD_MAX_FILESIZE" > /etc/php84/conf.d/05_upload_max_filesize.ini
-else
-    echo "upload_max_filesize = 128M" > /etc/php84/conf.d/05_upload_max_filesize.ini
-fi
+echo "memory_limit = $MEMORY_LIMIT" > /etc/php84/conf.d/05_memory_limit.ini
+echo "Set memory limit to $MEMORY_LIMIT"
+echo "upload_max_filesize = $UPLOAD_MAX_FILESIZE" > /etc/php84/conf.d/05_upload_max_filesize.ini
+echo "Set upload_max_filesize to $UPLOAD_MAX_FILESIZE"
+echo "post_max_size = $POST_MAX_SIZE" > /etc/php84/conf.d/05_post_max_size.ini
+echo "Set post_max_size to $POST_MAX_SIZE"
 
-if [ -n "$POST_MAX_SIZE" ]; then
-    echo "post_max_size = $POST_MAX_SIZE" > /etc/php84/conf.d/05_post_max_size.ini
-else
-    echo "post_max_size = 128M" > /etc/php84/conf.d/05_post_max_size.ini
-fi
-
+echo "Dumping env for composer"
 su -s /bin/sh www-data -c "composer dump-env prod"
 
-if [ "$RUN_MIGRATIONS" = "1" ]; then
+if [ "${RUN_MIGRATIONS:-0}" = "1" ]; then
+    echo "Running database migrations"
     su -s /bin/sh www-data -c "bin/console doctrine:migrations:migrate --all-or-nothing -n"
+else
+    echo "Skipping database migrations"
 fi
 
 exec "$@"
