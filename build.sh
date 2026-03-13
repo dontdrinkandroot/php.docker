d909be7a993cb43b34b245e7e3a479e7929d80f1
Philip Washington Sorst <philip@sorst.net>
2026-03-13 23:33:25 +0100

    refactor: Consolidating Dockerfiles

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 docker build --progress=plain --pull -t dontdrinkandroot/php:build-$BRANCH -f build/Dockerfile build/ \
-&& docker build --progress=plain -t dontdrinkandroot/php:symfony-base-$BRANCH -f symfony/base.Dockerfile symfony/ \
-&& docker build --progress=plain -t dontdrinkandroot/php:symfony-dev-$BRANCH -f symfony/dev.Dockerfile --build-arg FROM=dontdrinkandroot/php:symfony-base-$BRANCH symfony/ \
-&& docker build --progress=plain -t dontdrinkandroot/php:symfony-prod-$BRANCH -f symfony/prod.Dockerfile --build-arg FROM=dontdrinkandroot/php:symfony-base-$BRANCH symfony/
+&& docker build --progress=plain -t dontdrinkandroot/php:symfony-base-$BRANCH -f symfony/Dockerfile --target base symfony/ \
+&& docker build --progress=plain -t dontdrinkandroot/php:symfony-dev-$BRANCH -f symfony/Dockerfile --target dev symfony/ \
+&& docker build --progress=plain -t dontdrinkandroot/php:symfony-prod-$BRANCH -f symfony/Dockerfile --target prod symfony/
